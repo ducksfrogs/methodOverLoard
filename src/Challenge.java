c6983784265b0d9a2487669a825e57bb328c6e73
ducksfrogs <ma_yamaki@yahoo.com>
2025-02-06 07:43:10 +0900

    second commit

---
+++ src/Challenge.java
@@ -0,0 +1,18 @@
+public class Challenge {
+    public static void main(String[] args) {
+       System.out.println("5ft 8 = " + convertToCentimeters(5, 8) + "cm");
+        System.out.println("5 inche " + convertToCentimeters(5) + "cm");
+    }
+
+    public static double convertToCentimeters(int inches) {
+        return inches * 2.54;
+    }
+
+    public static double convertToCentimeters(int feet, int inches ) {
+        double v = convertToCentimeters((feet * 12) + inches);
+        return v;
+    }
+//    public static double convertToCentimeters(int feet, int inches ) {
+//        return ((feet * 12) + inches) * 2.54;
+//    }
+}
